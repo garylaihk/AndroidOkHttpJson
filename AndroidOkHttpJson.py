3afd291fbfa11e69ad7487a776180b728c42049d
Gary Lai <gary.lai@neoderm.com.hk>
2018-04-09 16:59:05 +0800

    Update Regex

--- AndroidOkHttpJson.py
+++ AndroidOkHttpJson.py
@@ -7,8 +7,8 @@ class AndroidOkHttpJsonCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		selection = sublime.Region(0, self.view.size())
 		selection_text = self.view.substr(selection)
-		text = re.sub(r'.*.OkHttp:.', '', selection_text, flags=re.MULTILINE)
-		text = re.sub(r"\n", '', text, flags=re.MULTILINE)
+		text = re.sub(r'(\n\s+)(.*.)', r'\2', selection_text, flags=re.MULTILINE)
+		text = re.sub(r'(.*.OkHttp:\s)(.*.)(\n{0,1})', r'\2', text, flags=re.MULTILINE)
 		self.view.replace(edit, selection, text)
 		try:
 			self.view.run_command("pretty_json_and_sort")
